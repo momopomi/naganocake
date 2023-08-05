be38d3eff595f86e14f28854a74659a03946abb4
EC2 Default User <132799867+momopomi@users.noreply.github.com>
2023-08-05 04:31:32 +0000

    add mypage

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,44 @@
+class Public::CustomersController < ApplicationController
+  
+  def show
+    @customer = current_customer
+    
+  end
+  def edit
+    @customer = current_customer
+  end
+  
+  def update
+    customer = Customer.find(params[:id])
+    unless customer.id == current_customer.id
+      redirect_to customer_path(current_customer.id)
+    end
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+        flash[:notice] = "edit successfully"
+        redirect_to customer_path(@customer.id)
+    else
+        render :edit
+    end
+  end
+  
+  def check
+    
+  end
+  
+  def withdraw
+    @customer = current_customer
+    @customer.is_deleted = true
+    if @customer.save
+      reset_session
+      redirect_to root_path
+    end
+  end
+  
+  
+
+
+  private
+
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,40 @@
+Rails.application.routes.draw do
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+namespace :admin do
+    resources :orders, only: [:show, :update]
+    get "/" => "homes#top"
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :items, only: [:index, :create, :new, :show, :edit, :update]
+    resources :order_details, only: [:update]
+    get "/search" => "items#search"
+  end
+
+scope module: :public do
+    get "/customers/check" => "customers#check"
+    patch "/customers/withdraw" => "customers#withdraw"
+    resource :customers, only: [:show, :update, :edit]
+    post "/orders/confirm" => "orders#confirm"
+    get "/orders/thanks" => "orders#thanks"
+    resources :orders, only: [:new, :create, :show, :index]
+    delete "/cart_items/destroy_all" => "cart_items#destroy_all"
+    resources :cart_items, only: [:index, :create, :destroy, :update]
+    resources :items, only: [:index, :show]
+    root "homes#top"
+    get "/about" => "homes#about"
+    get "/search" => "items#search"
+  end
+
+end
